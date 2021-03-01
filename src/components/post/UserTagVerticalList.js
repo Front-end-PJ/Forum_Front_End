14b686ebdcda7d4da54abd5e43978c64e230e861
ww8007 <wshmin@naver.com>
2021-03-01 23:24:26 +0900

    SideBar List

---
+++ src/components/post/UserTagVerticalList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const TagBlock = styled.div`
+  position: fixed;
+  top: 20%;
+  left: 10%;
+`;
+
+const UserTagVerticalList = ({ title }) => {
+  return <TagBlock>Tag</TagBlock>;
+};
+
+export default UserTagVerticalList;
