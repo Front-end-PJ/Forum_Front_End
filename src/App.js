743288d1c01d9ef4ce24df4912ee585a12d7781e
ww8007 <wshmin@naver.com>
2021-03-01 23:04:50 +0900

    Make Route Pages

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import { Route } from 'react-router-dom';
+import PostListPage from './pages/PostListPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import WritePage from './pages/WritePage';
+import PostPage from './pages/PostPage';
+import { Helmet } from 'react-helmet-async';
+
+function App() {
+  return (
+    <>
+      <Helmet>
+        <title>REACTERS</title>
+      </Helmet>
+      <Route
+        component={PostListPage}
+        path={['/board/:postId', '/board', '/']}
+        exact
+      />
+      <Route component={LoginPage} path="/login" />
+      <Route component={RegisterPage} path="/register" />
+      <Route component={WritePage} path="/write" />
+      <Route component={PostPage} path="/board/post/:postId" />
+    </>
+  );
+}
+
+export default App;
