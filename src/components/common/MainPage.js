e813a8573dcc9c20d2eeb0b23605db60f32308e6
ww8007 <wshmin@naver.com>
2021-03-01 23:10:15 +0900

    Make Presentaional MainPage

---
+++ src/components/common/MainPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+import Calendar from 'tui-calendar';
+const MainPage = () => {
+  return <div></div>;
+};
+
+export default MainPage;
