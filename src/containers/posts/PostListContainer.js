bedfd2e8d553e11819ce116490f5d5f166d554e4
ww8007 <wshmin@naver.com>
2021-03-19 11:36:52 +0900

    Fix npm

--- src/containers/posts/PostListContainer.js
+++ src/containers/posts/PostListContainer.js
@@ -5,64 +5,64 @@ import PostList from "../../components/posts/PostList";
 import { listPosts } from "../../modules/posts";
 
 const PostListContainer = ({ match, history }) => {
-  let { postId } = match.params;
-  const dispatch = useDispatch();
-  const { posts, error, loading, user, data, post, postsdata } = useSelector(
-    ({ posts, loading, user, post }) => ({
-      posts: posts.posts,
-      error: posts.error,
-      loading: loading["posts/READ_BOARD"],
-      user: user.user,
-      data: posts.data,
-      boards: posts.boards,
-      post: post.post,
-      _postId: post._postId,
-      postsdata: posts.postsdata,
-    })
-  );
-  // 각 게시판 별 게시물들 불러오기
-  useEffect(() => {
-    if (postId === undefined) {
-      postId = 1;
-    }
-    let id = postId;
-    let start = localStorage.getItem("start");
-    let end = localStorage.getItem("end");
+   let { postId } = match.params;
+   const dispatch = useDispatch();
+   const { posts, error, loading, user, data, post, postsdata } = useSelector(
+      ({ posts, loading, user, post }) => ({
+         posts: posts.posts,
+         error: posts.error,
+         loading: loading["posts/READ_BOARD"],
+         user: user.user,
+         data: posts.data,
+         boards: posts.boards,
+         post: post.post,
 
-    if (start === null || end === null) {
-      localStorage.setItem("start", 0);
-      localStorage.setItem("end", 10);
-      start = 0;
-      end = 10;
-    } else {
-      start = start.toString().replace(/"/g, "");
-      end = end.toString().replace(/"/g, "");
-    }
-    console.log("start, end", start, end);
-    dispatch(listPosts({ id, start, end }));
-  }, [dispatch, postId]);
-  useEffect(() => {
-    if (postId) {
-      try {
-        localStorage.setItem("postId", JSON.stringify(postId));
-      } catch (e) {
-        console.log("localStorage is not working");
+         postsdata: posts.postsdata,
+      })
+   );
+   // 각 게시판 별 게시물들 불러오기
+
+   if (postId === undefined) {
+      postId = 1;
+   }
+   useEffect(() => {
+      let id = postId;
+      let start = localStorage.getItem("start");
+      let end = localStorage.getItem("end");
+
+      if (start === null || end === null) {
+         localStorage.setItem("start", 0);
+         localStorage.setItem("end", 10);
+         start = 0;
+         end = 10;
+      } else {
+         start = start.toString().replace(/"/g, "");
+         end = end.toString().replace(/"/g, "");
       }
-    }
-  }, [history, postId]);
-  return (
-    <PostList
-      postId={postId}
-      loading={loading}
-      error={error}
-      posts={posts}
-      data={data}
-      post={post}
-      user={user}
-      postsdata={postsdata}
-      showWriteButton={user}
-    />
-  );
+      console.log("start, end", start, end);
+      dispatch(listPosts({ id, start, end }));
+   }, [dispatch, postId]);
+
+   if (postId) {
+      try {
+         localStorage.setItem("postId", JSON.stringify(postId));
+      } catch (e) {
+         console.log("localStorage is not working");
+      }
+   }
+   return (
+      <PostList
+         postId={postId}
+         loading={loading}
+         error={error}
+         posts={posts}
+         data={data}
+         post={post}
+         user={user}
+         postsdata={postsdata}
+         showWriteButton={user}
+      />
+   );
 };
 
 export default withRouter(PostListContainer);
